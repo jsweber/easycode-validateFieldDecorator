434670f61370a6ee465b5aa5501743b289a468bc
duxq <951092973@qq.com>
2020-02-01 15:49:26 +0800

    feat: init

---
+++ example/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<div>easy code</div>, document.getElementById('root'))
---
+++ scripts/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const NODE_ENV = process.env.NODE_ENV
+
+const config = {
+    entry: {
+        app: ['./src/index.js']
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/',
+        filename: 'validateFieldDecorator.js',
+        chunkFilename: '[id].js',
+        libraryTarget: 'umd',
+        library: 'validateFieldDecorator',
+    },
+    mode: NODE_ENV,
+    module: {
+        rules: [
+            {
+                test: /\.(js|mjs|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+}
+console.log('***********************' + NODE_ENV + '***********************')
+
+module.exports = config
