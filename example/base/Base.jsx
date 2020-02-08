ed689da6fa51f409bb75f2c2864b01683499bc40
duxq <951092973@qq.com>
2020-02-08 17:19:23 +0800

    docs: update example

--- example/base/Base.jsx
+++ example/base/Base.jsx
@@ -55,83 +55,99 @@ class App extends React.Component{
         return (
             <div className="base-wrapper">
                 <h2>base example</h2>
-                <InputWithValidate
-                    name="username"
-                    value={data.username}
-                    onChange= {this.changeValue('username')}
-                    required
-                    rules={{
-                        builtValidate: 'required', // Built-in Validation
-                        errMsg: 'required'
-                    }}
-                />
-
-                <InputWithValidate
-                    name="password"
-                    type="password"
-                    value={data.password}
-                    onChange= {this.changeValue('password')}
-                    required
-                    // multipe validater rules
-                    rules={[
-                        {
-                            builtValidate: 'required',
+                <div className="base-field">
+                    <span className="base-field-desc">1.required</span>
+                    <InputWithValidate
+                        name="username (required)"
+                        value={data.username}
+                        onChange= {this.changeValue('username')}
+                        required
+                        rules={{
+                            builtValidate: 'required', // Built-in Validation
                             errMsg: 'required'
-                        },
-                        {
-                            validate(value){
-                                return /^\d*$/.test(value)
+                        }}
+                    />
+                </div>
+                
+                <div className="base-field">
+                    <span className="base-field-desc">2.required and value must be number</span>
+                    <InputWithValidate
+                        name="password"
+                        type="password"
+                        value={data.password}
+                        onChange= {this.changeValue('password')}
+                        required
+                        // multipe validater rules
+                        rules={[
+                            {
+                                builtValidate: 'required',
+                                errMsg: 'required'
                             },
-                            errMsg: 'must be number'
-                        }
-                    ]}
-                />
-                <InputWithValidate
-                    name="local"
-                    value={data.local}
-                    onChange= {this.changeValue('local')}
-                    required
-                    debounce={300} // default 60
-                    // async validate
-                    rules={{
-                        asyncValidate(value){
-                            return new Promise( resolve => {
-                                setTimeout(() => {
-                                    if (value === 'ok') {
-                                        // 异步验证成功
-                                        resolve()
-                                    } else {
-                                        // 异步验证失败, resolve传错误信息
-                                        resolve('must be ok')
-                                    }
-                                }, 500)
-                            })
-                        }
-                    }}
-                />
-                <InputWithValidate
-                    name="age"
-                    value={data.age}
-                    onChange= {this.changeValue('age')}
-                    required
-                    rules={{
-                        builtValidate: 'max',
-                        param: 35,
-                        errMsg: 'age must be smaller than 35'
-                    }}
-                />
-                <RadioWithValidate
-                    name="sex"
-                    value={data.sex}
-                    onChange= {this.changeValue('sex')}
-                    required
-                    rules={{
-                        validate(value){
-                            return value === 'unkown'
-                        },
-                        errMsg: 'must be unkown'
-                    }}
-                />
+                            {
+                                validate(value){
+                                    return /^\d*$/.test(value)
+                                },
+                                errMsg: 'must be number'
+                            }
+                        ]}
+                    />
+                </div>
+                <div className="base-field">
+                    <span className="base-field-desc">3.async validate, value must be &#39;ok&#39;</span>
+                    <InputWithValidate
+                        name="local"
+                        value={data.local}
+                        onChange= {this.changeValue('local')}
+                        required
+                        debounce={300} // default 60
+                        // async validate
+                        rules={{
+                            asyncValidate(value){
+                                return new Promise( resolve => {
+                                    setTimeout(() => {
+                                        if (value === 'ok') {
+                                            // 异步验证成功
+                                            resolve()
+                                        } else {
+                                            // 异步验证失败, resolve传错误信息
+                                            resolve('must be ok')
+                                        }
+                                    }, 500)
+                                })
+                            }
+                        }}
+                    />
+                </div>
+                <div className="base-field">
+                    <span className="base-field-desc">4.age must be smaller than 35</span>
+                    <InputWithValidate
+                        name="age"
+                        value={data.age}
+                        onChange= {this.changeValue('age')}
+                        required
+                        rules={{
+                            builtValidate: 'max',
+                            param: 35,
+                            errMsg: 'age must be smaller than 35'
+                        }}
+                    />
+                </div>
+
+                <div className="base-field">
+                    <span className="base-field-desc">5.sex must be unkown</span>
+                    <RadioWithValidate
+                        name="sex"
+                        value={data.sex}
+                        onChange= {this.changeValue('sex')}
+                        required
+                        rules={{
+                            validate(value){
+                                return value === 'unkown'
+                            },
+                            errMsg: 'must be unkown'
+                        }}
+                    />
+                </div>
 
                 <button 
                     onClick={this.onSubmit}
